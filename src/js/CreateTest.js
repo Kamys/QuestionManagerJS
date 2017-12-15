afa43dfa05aa460a790341009f6bcf86e5aaf677
Nikita Kamyshenko <hnkntoc@gmail.com>
2017-12-16 02:20:54 +0300

    Update

--- src/js/CreateTest.js
+++ src/js/CreateTest.js
@@ -1,11 +1,12 @@
 $(function () {
-    const listQuestion = $('#list-question');
-    let currentItem;
-
-    const dataQuestion = [
+    const questionHist = [
         {question: "В каком году родился Пушкин A. С. ?", answer: "1799", point: 11},
         {question: "5 + 5 = ?", answer: "10", point: 25},
-        {question: "Столица Бельгии?", answer: "Брюссель", point: 5},
+        {question: "В каком году основан воронеж?", answer: "1586", point: 25},
+        {question: "Столица Бельгии?", answer: "Брюссель", point: 5}
+    ];
+
+    const questionInf = [
         {
             question: "Как называется группа файлов, которая хранится отдельной группой и имеет собственное имя?",
             answer: "Каталог",
@@ -21,9 +22,54 @@ $(function () {
         {question: "Какое наибольшее количество символов имеет расширение имени файла?", answer: "3", point: 16},
     ];
 
+    const dataTest = [
+        {id: 0, name: "История", maxPoint: 11, questions: questionHist},
+        {id: 1, name: "Информатика", maxPoint: 11, questions: questionInf}
+    ];
+
+    const listTest = $('#list-test');
+    const listQuestion = $('#list-question');
+    let currentItem;
+    let currentQuestions;
+
     initChangeInput();
     initListenerButton();
-    updateList();
+    initListTest();
+
+    updateList(dataTest, listTest, x => x.name, showTest);
+
+
+    function initListTest() {
+        listTest.empty();
+    }
+
+    function showTest(test) {
+        $('#test-input').val(test.name);
+        $('#max-points-input').val(test.maxPoint);
+
+        let questions = test.questions;
+        updateList(questions, listQuestion, q => q.question, showQuestion);
+        currentQuestions = questions;
+        if(questions.length !== 0){
+            showQuestion(questions[0]);
+        }
+    }
+
+    function updateList(listData, DDL, getName, click) {
+        DDL.empty();
+        listData.forEach(function (item, index) {
+            const tab = createDDLItem(index, getName(item));
+            DDL.append(tab)
+        });
+        $(".list-group-item").click(itemClick);
+
+        function itemClick() {
+            const index = $(this).attr('index');
+            let item = listData[index];
+            click(item);
+        }
+    }
+
 
     function initChangeInput() {
         $('#question-input').on('input', inputQuestion);
@@ -39,35 +85,23 @@ $(function () {
         $('#question-btn-save').click(saveQuestion);
         $('#question-btn-add').click(addQuestion);
         $('#question-btn-delete').click(deleteCurrentQuestion);
+
         $('#test-btn-save').click(saveTest);
     }
-    
+
     function saveTest() {
-        const testJson = JSON.stringify(dataQuestion);
+        const testJson = JSON.stringify(currentQuestions);
         $.cookie("test-data", testJson);
         toastr.success("Test save.");
     }
 
-    function updateList() {
-        listQuestion.empty();
-        dataQuestion.forEach(function (question, index) {
-            const tab = createTabQuestion(index, question.question);
-            listQuestion.append(tab)
-        });
-        $(".list-group-item").click(itemClick);
+
+    function selectQuestion(question) {
+        currentItem = question;
+        showQuestion(question);
     }
 
-    function itemClick() {
-        const index = $(this).attr('index');
-        selectQuestion(index);
-    }
-
-    function selectQuestion(index) {
-        currentItem = index;
-        showQuestion(dataQuestion[index]);
-    }
-
-    function createTabQuestion(index, name) {
+    function createDDLItem(index, name) {
         return $(`
         <a
             class="list-group-item list-group-item-action"
@@ -77,31 +111,35 @@ $(function () {
         >${name || "Имя вопроса"}</a>`);
     }
 
-    function showQuestion(itemQuestion) {
-        $('#question-input').val(itemQuestion.question);
-        $('#answer-input').val(itemQuestion.answer);
-        $('#point-input').val(itemQuestion.point);
+    function showQuestion(question) {
+        $('#question-input').val(question.question);
+        $('#answer-input').val(question.answer);
+        $('#point-input').val(question.point);
     }
 
     function addQuestion() {
-        dataQuestion.push({question: "Вопрос " + dataQuestion.length, answer: "", point: 0});
-        updateList();
-        selectQuestion(dataQuestion.length - 1);
+        currentQuestions.push({question: "Вопрос " + currentQuestions.length, answer: "", point: 0});
+        updateListCurrentQuestions();
+        selectQuestion(currentQuestions.length - 1);
     }
 
     function saveQuestion() {
-        dataQuestion[currentItem].question = $('#question-input').val();
-        dataQuestion[currentItem].answer = $('#answer-input').val();
-        dataQuestion[currentItem].point = $('#point-input').val();
-        updateList();
+        currentItem.question = $('#question-input').val();
+        currentItem.answer = $('#answer-input').val();
+        currentItem.point = $('#point-input').val();
+        updateListCurrentQuestions();
         $('#question-btn-save').prop('disabled', true);
     }
 
     function deleteCurrentQuestion() {
-        delete dataQuestion[currentItem];
-        updateList();
+        delete currentQuestions[currentItem];
+        updateListCurrentQuestions();
         $('#question-input').val('');
         $('#answer-input').val('');
         $('#point-input').val('');
+    }
+
+    function updateListCurrentQuestions(){
+        updateList(currentQuestions, listQuestion, q => q.question, selectQuestion);
     }
 });
